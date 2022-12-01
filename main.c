4fa9353e8061173ba6bd251a75dcd0712d4a1872
RealZiv <gintnergerman2004@gmail.com>
2022-12-01 21:56:29 +0700

    second commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+
+int main ()
+{
+    int f1();
+    int sum, k, a[11],i, p;
+   // a[11] = {10,11,12,13,14,15,16,17,18,19,20};
+    k=0;
+    sum=0;
+
+f1();
+    return 0;
+}
